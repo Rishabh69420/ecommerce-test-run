1ea46908c6647d554bcd96c604889c140321f7f6
Rishabh Chopde <rishikumarikuf@gmail.com>
2021-11-17 18:28:40 +0400

    changed to sass, reworked for respnsiveness

--- app.py
+++ app.py
@@ -42,5 +42,9 @@ def submit():
 
     return render_template('success.html')
 
+# @app.route("/lol")
+# def lol():
+#     return render_template('success.html')
+
 if __name__ == "__main__":
     app.run(debug=True)

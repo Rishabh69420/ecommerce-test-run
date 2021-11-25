9e4f98c63d6d65f9b2ca10dd508a369e19e25c0f
Rishabh Chopde <rishikumarikuf@gmail.com>
2021-11-25 13:10:45 +0400

    implemented google oauth

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
 import os
+import pathlib
+import requests
+from pip._vendor import cachecontrol
+import google
+from google.oauth2 import id_token
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, request, url_for, session
+from werkzeug.datastructures import cache_property
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from google_auth_oauthlib.flow import Flow
+from authlib.integrations.flask_client import OAuth
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{os.getenv("password")}@localhost/postgres'
+app.secret_key = f"{os.getenv('secret_key')}"
 
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GOOGLE_CLIENT_ID = "718120522137-o13a98t0jrn28i3lp1rotl2uspbjsvqu.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/authorize"
+)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{os.getenv("password")}@localhost/postgres'
 db = SQLAlchemy(app)
 
 
@@ -26,6 +46,8 @@ class Table(db.Model):
 
 @app.route("/")
 def index():
+    email = dict(session).get("email",None)
+    print(email)
     return render_template("index.html")
 
 @app.route('/submit', methods=['GET', 'POST'])
@@ -42,9 +64,43 @@ def submit():
 
     return render_template('success.html')
 
-# @app.route("/lol")
-# def lol():
-#     return render_template('success.html')
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+@app.route("/authorize")
+def authorize():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    chached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=chached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+    
+    return id_info
+
+# @app.route("/callback")
+# def callback():
+#     pass
+
+# @app.route("/index")
+# def index():
+#     pass
+
+# @app.route("/protected_area")
+# def protected_area():
+#     pass
 
 if __name__ == "__main__":
     app.run(debug=True)
